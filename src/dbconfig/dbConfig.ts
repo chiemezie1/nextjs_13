6dc2173a5d3bc81fe5a4ade4d4b033774a5b841c
chiemezie1 <chiemezieagbo1@gmail.com>
2023-10-04 20:49:43 +0100

    connection to mongodb

---
+++ src/dbconfig/dbConfig.ts
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+export async function connect(){
+    try {
+        await mongoose.connect(process.env.MONGO_URL!);
+        const connection = mongoose.connection;
+
+        connection.on('connected', () => {
+            console.log('Connected to MongoDB');
+        })
+        connection.on('error', (error) => {
+            console.log("Error connecting to MongoDB: ", error);
+        })
+    } catch (error) {
+        console.log(error);
+    }
+}
