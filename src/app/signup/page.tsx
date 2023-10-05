15e1ee5f09d378b2f543f44da8784c342ff38591
chiemezie1 <chiemezieagbo1@gmail.com>
2023-10-05 20:43:29 +0100

    sign up

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,6 +1,22 @@
+"use client";
+import Link from 'next/link';
 import React from 'react'
+import { useRouter } from 'next/navigation';
+import { axios } from 'axios';
 
-export default function page() {
+
+export default function SignupPage() {
+  const [user, setUser] = React.useState({
+    email: '',
+    password: '',
+    username: '',
+  }); 
+
+  const onSignup = async () => {
+    const res = await axios.post('/api/users/signup', user);
+    console.log(res);
+  }
+
   return (
     <div>
       <div>signup</div>
