34123c73df4bb0675d7555e02b127d0cc7cb753b
chiemezie1 <chiemezieagbo1@gmail.com>
2023-10-06 21:06:59 +0100

    user model

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,8 @@
+import {connect} from '@/dbconfig/dbConfig';
+import User from '@/models/userModel';
+
+
+
+
+
+connect();
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -3,11 +3,13 @@ import mongoose from "mongoose";
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true,
+        required: [true, "Please add a name"],
+        unique: true,
     },
     email: {
         type: String,
-        required: true,
+        required: [true, "Please add an email"],
+        unique: true,
     },
     password: {
         type: String,
@@ -17,6 +19,19 @@ const userSchema = new mongoose.Schema({
         type: Date,
         default: Date.now,
     },
+    isVerfied:{
+        type: Boolean,
+        default: false
+    },
+    isAdmin:{
+        type: Boolean,
+        default: false
+    },
+    forgotPasswordToken: String,
+    forgotPasswordExpiry: Date,
+    verifyEmailToken: String,
+    verifyEmailExpiry: Date,
+
 })
 
 const User = mongoose.model.users || mongoose.model("users", userSchema);
